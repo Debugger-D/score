1cb3093b21bb0e55bba8c669d9ca199bfe850570
Debugger-D <897443991@qq.com>
2019-01-03 15:18:21 +0800

    daily

--- src/views/daily/daily.vue
+++ src/views/daily/daily.vue
@@ -1,6 +1,6 @@
 <template>
-  <swiper :options="swiperOption" class="swiper-box">
-    <swiper-slide class="swiper-item">Slide 1</swiper-slide>
+  <div :options="swiperOption" class="swiper-box">
+   <!--  <swiper-slide class="swiper-item">Slide 1222222</swiper-slide>
     <swiper-slide class="swiper-item">Slide 2</swiper-slide>
     <swiper-slide class="swiper-item">Slide 3</swiper-slide>
     <swiper-slide class="swiper-item">Slide 4</swiper-slide>
@@ -11,11 +11,14 @@
     <swiper-slide class="swiper-item">Slide 9</swiper-slide>
     <swiper-slide class="swiper-item">Slide 10</swiper-slide>
     <div class="swiper-button-prev" slot="button-prev"></div>
-    <div class="swiper-button-next" slot="button-next"></div>
-  </swiper>
+    <div class="swiper-button-next" slot="button-next"></div> -->
+    <input @change="fileData" type="file">
+  </div>
 </template>
 
 <script>
+  import axios from 'axios';
+
   import { swiper, swiperSlide } from 'vue-awesome-swiper'
   export default {
     components: { swiper, swiperSlide },
@@ -32,6 +35,28 @@
           nextButton:'.swiper-button-next'
         }
       }
+    },
+    methods: {
+      fileData (obj) {
+        let fileList = obj.target.files
+        let file = fileList[0]
+        console.log(file)
+        let instance = axios.create({
+          baseURL: 'http://cms.landi.com:4000/',
+          headers: {'Content-Type':'multipart/form-data'},
+          withCredentials: true
+        })
+        instance.post('/api/upload', file).then((data) => {
+          console.log(data)
+        })
+        // let url = window.URL || window.webkitUrl
+        /* let imgUrl = URL.createObjectURL(file)
+        let textArea = document.getElementsByClassName('textarea')[0]
+        let img = document.createElement('img')
+        img.src = imgUrl
+        console.log(img)
+        textArea.appendChild(img) */
+      },
     }
   }
 </script>
